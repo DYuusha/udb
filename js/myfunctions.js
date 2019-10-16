c0d81820a9c40a0ed67651a1ed12f9e6df82a369
Mauricio Trejo <mauriciotrejo.013@gmail.com>
2019-10-16 00:30:26 -0600

    specs carousel

---
+++ js/myfunctions.js
@@ -0,0 +1,52 @@
+showIlustration = () => {
+    let il = document.getElementById('carousel-ilustracion')
+    il.style.visibility = 'visible';
+}
+
+hideIlustration = () => {
+
+    let il = document.getElementById('carousel-ilustracion')
+    il.style.visibility = 'hidden';
+}
+
+
+showDigital = () => {
+    let il = document.getElementById('carousel-digital')
+    il.style.visibility = 'visible';
+}
+
+hideDigital = () => {
+
+    let il = document.getElementById('carousel-digital')
+    il.style.visibility = 'hidden';
+}
+showUXUI = () => {
+    let il = document.getElementById('carousel-uxui')
+    il.style.visibility = 'visible';
+}
+
+hideUXUI = () => {
+
+    let il = document.getElementById('carousel-uxui')
+    il.style.visibility = 'hidden';
+}
+showEditorial = () => {
+    let il = document.getElementById('carousel-editorial')
+    il.style.visibility = 'visible';
+}
+
+hideEditorial = () => {
+
+    let il = document.getElementById('carousel-editorial')
+    il.style.visibility = 'hidden';
+}
+showBranding = () => {
+    let il = document.getElementById('carousel-branding')
+    il.style.visibility = 'visible';
+}
+
+hideBranding = () => {
+
+    let il = document.getElementById('carousel-branding')
+    il.style.visibility = 'hidden';
+}
